1ec4bbc67e383642124665d5029dbf9fe718dd5b
KotaYamaguchi <k-yamaguchi@ilab.pu-kumamoto.ac.jp>
2025-07-14 18:19:38 +0900

    ページ機能追加

---
+++ Function Card App/PageManager.swift
@@ -0,0 +1,19 @@
+//
+//  CanvasManager.swift
+//  Function Card App
+//
+//  Created by 山口昂大 on 2025/07/14.
+//
+
+import SwiftUI
+
+struct CanvasManager: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CanvasManager()
+}
+
